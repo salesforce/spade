ab5167b2e86a6ffd6ae0e0d534f49540dd5c247d
Kexin Xie <kexin.xie@salesforce.com>
2022-11-03 15:52:42 -0700

    Refactor context to allow easier customization

--- spade-aws/src/main/scala/com/salesforce/mce/spade/aws/SpadeAwsContext.scala
+++ spade-aws/src/main/scala/com/salesforce/mce/spade/aws/SpadeAwsContext.scala
@@ -9,49 +9,51 @@ package com.salesforce.mce.spade.aws
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-class SpadeAwsContext private (config: Config) {
-
-  val emr: SpadeAwsContext.Emr = SpadeAwsContext.Emr.withRootConfig(config)
-
-}
+case class SpadeAwsContext(emr: SpadeAwsContext.Emr)
 
 object SpadeAwsContext {
 
-  class Emr private (config: Config) {
-
-    def releaseLabel: String = config.getString("release-label")
-
-    def subnetId: String = config.getString("subnet-id")
-
-    def ec2KeyName: String = config.getString("ec2-keyname")
-
-    def instanceCount: Int = config.getInt("instance-count")
-
-    def masterInstanceType: String = config.getString("master-instance-type")
-
-    def slaveInstanceType: String = config.getString("slave-instance-type")
-
-    def serviceRole: String = config.getString("service-role")
-
-    def resourceRole: String = config.getString("resource-role")
-
-    def tags: Seq[(String, String)] = Seq.empty
-
-  }
+  case class Emr(
+    releaseLabel: String,
+    subnetId: String,
+    ec2KeyName: String,
+    instanceCount: Int,
+    masterInstanceType: String,
+    slaveInstanceType: String,
+    serviceRole: String,
+    resourceRole: String,
+    tags: Seq[(String, String)]
+  )
 
   object Emr {
     def configPath = "emr"
 
-    def withRootConfig(rootConfig: Config): Emr =
-      new Emr(rootConfig.getConfig(configPath))
+    def withRootConfig(rootConfig: Config): Emr = {
+      val config = rootConfig.getConfig(configPath)
+
+      Emr(
+        config.getString("release-label"),
+        config.getString("subnet-id"),
+        config.getString("ec2-keyname"),
+        config.getInt("instance-count"),
+        config.getString("master-instance-type"),
+        config.getString("slave-instance-type"),
+        config.getString("service-role"),
+        config.getString("resource-role"),
+        Seq.empty
+      )
+    }
 
     def apply(): Emr = withRootConfig(ConfigFactory.load())
   }
 
   def configPath = "com.salesforce.mce.spade.aws"
 
-  def withRootConfig(rootConfig: Config): SpadeAwsContext =
-    new SpadeAwsContext(rootConfig.getConfig(configPath))
+  def withRootConfig(rootConfig: Config): SpadeAwsContext = {
+    val config = rootConfig.getConfig(configPath)
+    val emr = Emr.withRootConfig(config)
+    SpadeAwsContext(emr)
+  }
 
   def apply(): SpadeAwsContext = withRootConfig(ConfigFactory.load())
 
