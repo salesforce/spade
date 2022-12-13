c073a7e0c472669d58f9b80b63c076e4bc061994
Annie Zhang <37345842+anniezhang77@users.noreply.github.com>
2022-12-12 19:27:27 -0800

    Add support for ec2 and shell script activity (#5)

---
+++ spade-aws/src/main/scala/com/salesforce/mce/spade/aws/resource/Ec2Instance.scala
@@ -0,0 +1,62 @@
+package com.salesforce.mce.spade.aws.resource
+
+import java.util.UUID
+
+import io.circe.syntax._
+
+import com.salesforce.mce.spade.SpadeContext
+import com.salesforce.mce.spade.aws.SpadeAwsContext
+import com.salesforce.mce.spade.aws.spec.{AwsTag, Ec2ResourceSpec}
+import com.salesforce.mce.spade.workflow.Resource
+
+trait Ec2Instance
+
+object Ec2Instance {
+
+  final val ResourceType = "aws.resource.Ec2Resource"
+
+  case class BootstrapAction(path: String, args: String*)
+
+  case class Builder(
+    nameOpt: Option[String],
+    instanceType: Option[String],
+    securityGroupIds: Option[Seq[String]],
+    spotInstance: Option[Boolean],
+    maxAttempt: Option[Int]
+  ) {
+
+    def withName(name: String) = copy(nameOpt = Option(name))
+
+    def withInstanceType(instType: String) = copy(instanceType = Option(instType))
+
+    def withSecurityGroupIds(groupIds: String*) = copy(securityGroupIds = Option(groupIds))
+
+    def withMaxAttempt(n: Int) = copy(maxAttempt = Option(n))
+
+    def build()(implicit ctx: SpadeContext, sac: SpadeAwsContext): Resource[Ec2Instance] = {
+
+      val id = UUID.randomUUID().toString()
+      val name = nameOpt.getOrElse(s"Ec2Instance-$id")
+
+      Resource[Ec2Instance](
+        id,
+        name,
+        ResourceType,
+        Ec2ResourceSpec(
+          sac.ec2.amiImageId,
+          sac.ec2.subnetId,
+          instanceType.getOrElse(sac.ec2.instanceType),
+          sac.ec2.instanceProfile,
+          securityGroupIds,
+          Option((sac.tags.toSeq ++ sac.ec2.tags).distinct.map { case (k, v) => AwsTag(k, v) }),
+          spotInstance.getOrElse(sac.ec2.spotInstance)
+        ).asJson,
+        maxAttempt.getOrElse(ctx.maxAttempt)
+      )
+    }
+  }
+
+  def builder(): Ec2Instance.Builder =
+    Builder(None, None, None, None, None)
+
+}
--- spade-examples/src/main/scala/com/salesforce/mce/spade/examples/ExampleWorkflow.scala
+++ spade-examples/src/main/scala/com/salesforce/mce/spade/examples/ExampleWorkflow.scala
@@ -2,8 +2,8 @@ package com.salesforce.mce.spade.examples
 
 import com.salesforce.mce.spade.SpadePipeline
 import com.salesforce.mce.spade.aws.SpadeAwsContext
-import com.salesforce.mce.spade.aws.activity.{EmrActivity, EmrStep}
-import com.salesforce.mce.spade.aws.resource.EmrCluster
+import com.salesforce.mce.spade.aws.activity.{EmrActivity, EmrStep, ShellScriptActivity}
+import com.salesforce.mce.spade.aws.resource.{Ec2Instance, EmrCluster}
 import com.salesforce.mce.spade.cli.SpadeCli
 import com.salesforce.mce.spade.workflow.WorkflowExpression
 
@@ -16,6 +16,9 @@ object ExampleWorkflow extends SpadePipeline with SpadeCli {
   val act1 = EmrActivity.builder(emrCluster).withSteps(EmrStep("s3://somebucket/some.jar")).build()
   val act2 = EmrActivity.builder(emrCluster).withSteps(EmrStep("s3://somebucket/some.jar")).build()
 
-  override def workflow: WorkflowExpression = act1 ~> act2
+  val ec2Instance = Ec2Instance.builder().build()
+  val act3 = ShellScriptActivity.builder(ec2Instance, "s3://somebucket/script.sh", "somebucket", "output").build()
+
+  override def workflow: WorkflowExpression = act1 ~> act2 ~> act3
 
 }
